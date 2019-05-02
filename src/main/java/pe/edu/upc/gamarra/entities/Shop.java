fc5b09e226851c9b65ee49097eae2e52a37b1dd2
rodcar <ivanrod12@hotmail.com>
2019-05-02 11:31:29 -0500

    Agrega la relación entre las entidades Shop y Gallery

--- src/main/java/pe/edu/upc/gamarra/entities/Shop.java
+++ src/main/java/pe/edu/upc/gamarra/entities/Shop.java
@@ -2,10 +2,14 @@ package pe.edu.upc.gamarra.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="shops")
@@ -29,6 +33,11 @@ public class Shop {
 	
 	@Column(name="URLPhoto", nullable=true, length=700)
 	private String URLPhoto;
+	
+	@NotNull(message="Debe seleccionar una galería")
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="galleries_id", nullable=false)
+	private Gallery galleryId;
 
 	public Long getId() {
 		return id;
@@ -38,11 +47,52 @@ public class Shop {
 		this.id = id;
 	}
 
-	public String getNombre() {
-		return nombre;
+	public String getAddress() {
+		return address;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDirections() {
+		return directions;
+	}
+
+	public void setDirections(String directions) {
+		this.directions = directions;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getURLPhoto() {
+		return URLPhoto;
+	}
+
+	public void setURLPhoto(String uRLPhoto) {
+		URLPhoto = uRLPhoto;
+	}
+
+	public Gallery getGalleryId() {
+		return galleryId;
+	}
+
+	public void setGalleryId(Gallery galleryId) {
+		this.galleryId = galleryId;
+	}
+
 }
