2978bd594cafd055276bb2ffce7f8cadf379df4e
rodcar <ivanrod12@hotmail.com>
2019-06-11 16:13:04 -0500

    Agrega enviar Bearer JWT de autorización

--- src/main/java/pe/edu/upc/gamarra/config/SwaggerConfig.java
+++ src/main/java/pe/edu/upc/gamarra/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.gamarra.config;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +10,13 @@ import com.google.common.base.Predicates;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -27,10 +31,23 @@ public class SwaggerConfig {
 			"PREMIUM", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0",
 			new ArrayList<VendorExtension>());
 
+	@SuppressWarnings("deprecation")
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO).select() 
-		          .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-		             .build();
-	}
+    public SecurityConfiguration securityInfo() {
+        return new SecurityConfiguration(null, null, null, null, "", ApiKeyVehicle.HEADER,"Authorization","");
+    }
+	
+    @Bean
+    public Docket api() {
+    	return new Docket(DocumentationType.SWAGGER_2)
+    			.select() 
+		          .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))		         
+		          .build()
+		          .apiInfo(DEFAULT_API_INFO)
+		          .securitySchemes(Arrays.asList(apiKey()));
+    }
+    
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer", "Authorization", "header");
+    }
 }
